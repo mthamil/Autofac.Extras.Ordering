8e32ea663f98a51444f532272cef7f56b2e67c0a
Matthew Hamilton <matthamilton@live.com>
2014-05-29 23:07:10 -0500

    Renamed parameter.

--- Autofac.Extras.Ordering/OrderedRegistrationExtensions.cs
+++ Autofac.Extras.Ordering/OrderedRegistrationExtensions.cs
@@ -51,12 +51,12 @@ namespace Autofac.Extras.Ordering
         /// <typeparam name="TLimit">Registration limit type.</typeparam>
         /// <typeparam name="TRegistrationStyle">Registration style.</typeparam>
         /// <param name="registration">Registration to set parameter on.</param>
-        /// <param name="starting">An optional starting order</param>
+        /// <param name="startingWith">An optional starting order</param>
         /// <returns>A registration builder allowing further configuration of the component.</returns>
         public static IRegistrationBuilder<TLimit, ScanningActivatorData, TRegistrationStyle> OrderByRegistration<TLimit, TRegistrationStyle>(
-            this IRegistrationBuilder<TLimit, ScanningActivatorData, TRegistrationStyle> registration, int starting = 1)
+            this IRegistrationBuilder<TLimit, ScanningActivatorData, TRegistrationStyle> registration, int startingWith = 1)
         {
-            int order = starting;
+            int order = startingWith;
             registration.ActivatorData.ConfigurationActions.Add((type, builder) => 
                 builder.OrderBy(order++));
             return registration;
